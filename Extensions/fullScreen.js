7895b1e8d4c77a944129fc3b4c09842fbd41427e
daksh2k <daksh2k@gmail.com>
2021-12-23 16:04:09 +0530

    change activation button again!!!! Add option to reset config and reload client

--- Extensions/fullScreen.js
+++ Extensions/fullScreen.js
@@ -2012,7 +2012,29 @@ ${CONFIG[ACTIVE].lyricsDisplay ? `<div id="fad-lyrics-plus-container"></div>` : 
                      1: "Full"
                  },
                  "backgroundBrightness" in CONFIG[ACTIVE] ? Number(CONFIG[ACTIVE].backgroundBrightness) : .8 ,
-                 (value) => saveOption("backgroundBrightness",value))
+                 (value) => saveOption("backgroundBrightness",value)),
+            (() => {
+                const container = document.createElement("div");
+                container.innerHTML = `
+                <div class="setting-row-but">
+                  <button class="main-buttons-button main-button-secondary" id="reset-switch">Reset Config</button>
+                  <button class="main-buttons-button main-button-secondary" id="reload-switch">Reload Client</button>
+                </div>`;
+                container.querySelector("#reset-switch").onclick = () => {
+                  CONFIG[ACTIVE] = {}
+                  saveConfig()
+                  render()
+                  if (document.body.classList.contains('fsd-activated')) {
+                    activate()
+                  }
+                  configContainer = ""
+                  setTimeout(openConfig,5)
+                };
+                container.querySelector("#reload-switch").onclick = () => {
+                  location.reload()
+                };
+                return container;
+            })()
         )
         Spicetify.PopupModal.display({
             title: ACTIVE==="tv" ? "TV Mode Config" : "Full Screen Config",
@@ -2025,7 +2047,7 @@ ${CONFIG[ACTIVE].lyricsDisplay ? `<div id="fad-lyrics-plus-container"></div>` : 
     // Add Full Screen Button on bottom bar
     const button = document.createElement("button")
     button.classList.add("button", "fsd-button","control-button","InvalidDropTarget")
-    button.innerHTML = `<svg role="img" height="20" width="20" viewBox="0 0 24 24" fill="currentColor"><rect fill="none" height="24" width="24"/><polygon points="21,11 21,3 13,3 16.29,6.29 6.29,16.29 3,13 3,21 11,21 7.71,17.71 17.71,7.71"/></svg>`
+    button.innerHTML = `<svg role="img" height="20" width="20" viewBox="0 0 20 20" fill="currentColor"><path d="M10.5 3L16.5428 3.00182L16.6281 3.01661L16.691 3.03779L16.767 3.07719L16.8221 3.11759L16.8824 3.17788L16.9112 3.21534L16.9533 3.2886L16.9834 3.37186L16.9979 3.45421L17 3.5V9.5C17 9.77614 16.7761 10 16.5 10C16.2545 10 16.0504 9.82312 16.0081 9.58988L16 9.5V4.706L4.706 16H9.5C9.74546 16 9.94961 16.1769 9.99194 16.4101L10 16.5C10 16.7455 9.82312 16.9496 9.58988 16.9919L9.5 17L3.47964 16.9996L3.4112 16.9921L3.30896 16.9622L3.23299 16.9228L3.17786 16.8824L3.11758 16.8221L3.08884 16.7847L3.04674 16.7114L3.01661 16.6281L3.01109 16.605C3.00383 16.5713 3 16.5361 3 16.5L3.00546 16.5739L3.00182 16.5428L3 10.5C3 10.2239 3.22386 10 3.5 10C3.74546 10 3.94961 10.1769 3.99194 10.4101L4 10.5V15.292L15.292 4H10.5C10.2545 4 10.0504 3.82312 10.0081 3.58988L10 3.5C10 3.22386 10.2239 3 10.5 3Z"></path><path class="" d="M16 3C16.5523 3 17 3.44772 17 4V9.25C17 9.66421 16.6642 10 16.25 10C15.8358 10 15.5 9.66421 15.5 9.25V5.559L5.559 15.5H9.25C9.66421 15.5 10 15.8358 10 16.25C10 16.6642 9.66421 17 9.25 17H4C3.44772 17 3 16.5523 3 16V10.75C3 10.3358 3.33579 10 3.75 10C4.16421 10 4.5 10.3358 4.5 10.75V14.439L14.439 4.5H10.75C10.3358 4.5 10 4.16421 10 3.75C10 3.33579 10.3358 3 10.75 3H16Z"></path></svg>`
     button.id = "fs-button"
     button.setAttribute("title", "Full Screen")
 
