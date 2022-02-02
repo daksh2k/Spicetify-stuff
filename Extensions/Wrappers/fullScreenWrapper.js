cd26b5fe68968a1f563ffe6acb34326c89314d95
daksh2k <daksh2k@gmail.com>
2022-02-02 16:17:50 +0530

    Add: Wrappers for automatic updating extensions

---
+++ Extensions/Wrappers/fullScreenWrapper.js
@@ -0,0 +1,8 @@
+function addScript(src){
+  let script = document.createElement('script')
+  script.src = src
+  script.defer = true
+  script.type = 'text/javascript'
+  document.getElementsByTagName('head')[0].appendChild(script)
+}
+addScript("https://daksh2k.github.io/Spicetify-stuff/Extensions/fullScreen.js")
