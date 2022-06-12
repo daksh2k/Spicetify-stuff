bf9fc0bf326802db630edc052c1648beece5ee82
beltra <gbeve2002@gmail.com>
2022-06-12 23:06:10 +0200

    Added translations and more functionalities

--- Extensions/fullScreen.js
+++ Extensions/fullScreen.js
@@ -122,7 +122,9 @@ const translations = {
             
             cgfReset: "Reset config",
             reload: "Reload client"
-        }
+        },
+        tvBtnDesc: "TV mode",
+        fullscreenBtnDesc: "Fullscreen"
     },
     "it-IT": {
         langName: "Italiano",
@@ -185,7 +187,7 @@ const translations = {
             showAllArtistsUpNext: "Mostra tutti gli artisti in Up next",
             icons: "Icone",
             songChangeAnimation: "Animazione cambio brano",
-            fullscreen: "Fullscreen",
+            fullscreen: "Schermo intero",
 
             extraHeader: "Funzionalità aggiuntive",
             backgroundChoice: {
@@ -239,7 +241,9 @@ const translations = {
             
             cgfReset: "Resetta configurazione",
             reload: "Ricarica client"
-        }
+        },
+        tvBtnDesc: "Modalità TV",
+        fullscreenBtnDesc: "Schermo intero"
     }
 }
 
@@ -2888,7 +2892,7 @@ ${CONFIG[ACTIVE].lyricsDisplay ? `<div id="fad-lyrics-plus-container"></div>` : 
     const button = document.createElement("button");
     button.classList.add("button", "fsd-button", "control-button", "InvalidDropTarget");
     button.id = "fs-button";
-    button.setAttribute("title", "Schermo intero");
+    button.setAttribute("title", translations[LOCALE].settings.fullscreenBtnDesc);
 
     button.onclick = openwithDef;
 
@@ -2916,7 +2920,7 @@ ${CONFIG[ACTIVE].lyricsDisplay ? `<div id="fad-lyrics-plus-container"></div>` : 
     button2.classList.add("button", "tm-button", "main-topBar-button", "InvalidDropTarget");
     button2.innerHTML = `<svg role="img" height="16" width="16" viewBox="0 0 16 16" fill="currentColor"><path d="M2.5 13.5A.5.5 0 0 1 3 13h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zM13.991 3l.024.001a1.46 1.46 0 0 1 .538.143.757.757 0 0 1 .302.254c.067.1.145.277.145.602v5.991l-.001.024a1.464 1.464 0 0 1-.143.538.758.758 0 0 1-.254.302c-.1.067-.277.145-.602.145H2.009l-.024-.001a1.464 1.464 0 0 1-.538-.143.758.758 0 0 1-.302-.254C1.078 10.502 1 10.325 1 10V4.009l.001-.024a1.46 1.46 0 0 1 .143-.538.758.758 0 0 1 .254-.302C1.498 3.078 1.675 3 2 3h11.991zM14 2H2C0 2 0 4 0 4v6c0 2 2 2 2 2h12c2 0 2-2 2-2V4c0-2-2-2-2-2z"/></svg>`;
     button2.id = "TV-button";
-    button2.setAttribute("title", "Modalità TV");
+    button2.setAttribute("title", translations[LOCALE].settings.tvBtnDesc);
 
     button2.onclick = openwithTV;
 
