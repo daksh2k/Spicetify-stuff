47fa960a6515ea825f6809daae0fa96e379d384c
daksh2k <daksh2k@gmail.com>
2023-09-01 19:44:32 +0530

    fix(play-next): update queue adding logic

--- Extensions/playNext.js
+++ Extensions/playNext.js
@@ -52,7 +52,10 @@
             },
         });
         const albumDetails = await res.json();
-        return [...albumDetails.items.map((item) => item.uri), ...(!!albumDetails.next ? await fetchAlbumFromWebApi(albumDetails.next) : [])];
+        return [
+            ...albumDetails.items.map((item) => item.uri),
+            ...(!!albumDetails.next ? await fetchAlbumFromWebApi(albumDetails.next) : []),
+        ];
     };
 
     const fetchPlaylist = async (uri) => {
@@ -107,7 +110,9 @@
                 tracks = await fetchPlaylist(uri);
                 break;
             case Spicetify.URI.Type.ALBUM:
-                tracks = await fetchAlbumFromWebApi(`https://api.spotify.com/v1/albums/${uri.split(":")[2]}/tracks?limit=50`);
+                tracks = await fetchAlbumFromWebApi(
+                    `https://api.spotify.com/v1/albums/${uri.split(":")[2]}/tracks?limit=50`
+                );
                 break;
         }
         if (Spicetify.Player.getShuffle()) tracks = shuffle(tracks);
@@ -124,32 +129,32 @@
             Spicetify.showNotification("Malformed uris!");
             return;
         }
-        const newTracks = uris.map((uri) => ({
-            uri,
-            provider: "queue",
-            metadata: {
-                is_queued: true,
-            },
-        }));
-        await Spicetify.CosmosAsync.put("sp://player/v2/main/queue", {
-            queue_revision: Spicetify.Queue?.queueRevision,
-            next_tracks: [
-                ...newTracks,
-                ...Spicetify.Queue?.nextTracks.map((track) => ({
-                    uri: track.contextTrack.uri,
-                    provider: track.provider,
-                    metadata: {
-                        is_queued: track.provider === "queue",
-                    },
-                })),
-            ],
-            prev_tracks: Spicetify.Queue?.prevTracks,
-        })
-            .then(() => Spicetify.showNotification("Added to Play Next"))
-            .catch((err) => {
-                console.error("Failed to add to queue", err);
-                Spicetify.showNotification("Unable to Add! Check Console.");
-            });
+
+        const currentQueueLength = (Spicetify.Queue.nextTracks || []).filter(
+            (track) => track.provider !== "context"
+        ).length;
+
+        await Spicetify.addToQueue(uris.map((uri) => ({ uri }))).catch((err) => {
+            console.error("Failed to add to queue", err);
+        });
+
+        const newTracks = Spicetify.Queue.nextTracks
+            .filter((track) => track.provider !== "context")
+            .filter((_, index) => index >= currentQueueLength);
+
+        if (currentQueueLength) {
+            await Spicetify.Platform.PlayerAPI.reorderQueue(
+                newTracks.map((track) => track.contextTrack),
+                { before: Spicetify.Queue.nextTracks[0].contextTrack }
+            )
+                .then(() => Spicetify.showNotification("Added to Play Next"))
+                .catch((err) => {
+                    console.error("Failed to add to queue", err);
+                    Spicetify.showNotification("Unable to Add! Check Console.");
+                });
+        } else {
+            Spicetify.showNotification("Added to Play Next");
+        }
     }
 
     // Add option to Context Menu
