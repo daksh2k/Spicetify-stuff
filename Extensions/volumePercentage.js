550fd9be350c98b8cff86b06d6b98ee171e533e0
Grigory <techuila.sunset@gmail.com>
2023-11-25 15:39:06 +0500

    fix(Extensions/volumePercentage): adapt to changes in spotify v1.2.25

--- Extensions/volumePercentage.js
+++ Extensions/volumePercentage.js
@@ -8,21 +8,33 @@
 
 (function addVolumep() {
     const volumeBar = document.querySelector(".main-nowPlayingBar-volumeBar");
-    if (!(volumeBar instanceof HTMLElement) || !Spicetify.Player || !Spicetify.Platform?.PlayerAPI) {
+    const volumeProgressBar = volumeBar?.querySelector(".progress-bar");
+
+    if (!(volumeBar instanceof HTMLElement) || !(volumeProgressBar instanceof HTMLDivElement)) {
         setTimeout(addVolumep, 200);
         return;
     }
-    const ele = document.createElement("span");
-    ele.classList.add("volume-percent");
-    ele.setAttribute("style", "font-size: 14px; padding-left: 10px; min-width: 45px;");
 
-    volumeBar.append(ele);
+    const volumeProgressBarStyle = volumeProgressBar.style;
+
+    const volumePercent = document.createElement("span");
+    volumePercent.classList.add("volume-percent");
+    volumePercent.setAttribute("style", "font-size: 14px; padding-left: 10px; min-width: 45px;");
+    volumeBar.append(volumePercent);
     volumeBar.style.flex = "0 1 180px";
 
-    updatePercentage();
     function updatePercentage() {
-        const currVolume = Math.round(Spicetify.Player?.getVolume() * 100);
-        ele.innerText = currVolume == -100 ? `` : `${currVolume}%`;
+        const currentVolume = Math.round(
+            parseFloat(volumeProgressBarStyle.getPropertyValue("--progress-bar-transform")),
+        );
+        volumePercent.innerText = `${currentVolume}%`;
     }
-    Spicetify.Platform.PlayerAPI._events.addListener("volume", updatePercentage);
+    updatePercentage();
+
+    const progressBarObserver = new MutationObserver(updatePercentage);
+    const progressBarObserverConfig = {
+        attributes: true,
+        attributeFilter: ["style"],
+    };
+    progressBarObserver.observe(volumeProgressBar, progressBarObserverConfig);
 })();
