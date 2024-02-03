cdbe405a58411be15696c8bef9786a7709a14c32
York <goog10216922@gmail.com>
2024-02-03 12:21:19 +0800

    Add Chinese  Traditional translation

--- Extensions/full-screen/src/resources/strings.ts
+++ Extensions/full-screen/src/resources/strings.ts
@@ -9,22 +9,24 @@ import esES from "./locales/es-ES.json";
 import frFR from "./locales/fr-FR.json";
 import idID from "./locales/id-ID.json";
 import ptBR from "./locales/pt-BR.json";
+import zhTW from "./locales/zh-TW.json";
 import defaultsDeep from "lodash.defaultsdeep";
 
 // Translation strings
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const translations: Record<string, any> = {
-    "en-US": enUS,
-    "it-IT": defaultsDeep(itIT, enUS),
-    "zh-CN": defaultsDeep(zhCN, enUS),
-    "de-DE": defaultsDeep(deDE, enUS),
-    "tr-TR": defaultsDeep(trTR, enUS),
-    "ru-RU": defaultsDeep(ruRU, enUS),
-    "vi-VN": defaultsDeep(viVN, enUS),
-    "es-ES": defaultsDeep(esES, enUS),
-    "fr-FR": defaultsDeep(frFR, enUS),
-    "id-ID": defaultsDeep(idID, enUS),
-    "pt-BR": defaultsDeep(ptBR, enUS),
+  "en-US": enUS,
+  "it-IT": defaultsDeep(itIT, enUS),
+  "zh-CN": defaultsDeep(zhCN, enUS),
+  "de-DE": defaultsDeep(deDE, enUS),
+  "tr-TR": defaultsDeep(trTR, enUS),
+  "ru-RU": defaultsDeep(ruRU, enUS),
+  "vi-VN": defaultsDeep(viVN, enUS),
+  "es-ES": defaultsDeep(esES, enUS),
+  "fr-FR": defaultsDeep(frFR, enUS),
+  "id-ID": defaultsDeep(idID, enUS),
+  "pt-BR": defaultsDeep(ptBR, enUS),
+  "zh-TW": defaultsDeep(zhTW, enUS),
 };
 
 export default translations;
